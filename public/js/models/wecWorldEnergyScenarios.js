c9c68aec61511088235c7d975ed18d1b0c3ff861
Dan Lopuch <dan@datahero.com>
2016-10-17 17:02:11 -0700

    Adding transportation interpolations

--- public/js/models/wecWorldEnergyScenarios.js
+++ public/js/models/wecWorldEnergyScenarios.js
@@ -37,21 +37,131 @@ const ELECTRICITY_SOURCE_2060_GROWTHS = _.mapValues(ELECTRICITY_TWH_BY_SOURCE_AN
   hardRock          : modelsTwh[3] / modelsTwh[0],
 }));
 
+
+const TRANSPORT_CONSUMPTION_MTOE = {
+  // 2014, 2030, and 2060 energy demand, in MTOE
+  modernJazz: [2619, 3179, 3423], // page 44
+  unfinishedSymphony: [2619, 3050, 3123], // page 65
+  hardRock: [2619, 3256, 3904], // page 83
+};
+
+// const TRANSPORT_CONSUMPTION_GROWTH_PCT = _.mapValues(TRANSPORT_CONSUMPTION_MTOE, years => ({
+//   2030: years[1] / years[0],
+//   2060: years[2] / years[0],
+// }));
+
+const TRANSPORT_SHARE = {
+  modernJazz: {
+    // page 44
+    2014: {
+      petroleum: 0.92,
+      naturalGas: 0.04,
+      biomass: 0.03,
+      electricity: 0.01,
+    },
+    2030: {
+      petroleum: 0.90,
+      naturalGas: 0.03,
+      biomass: 0.05,
+      electricity: 0.02,
+    },
+    2060: {
+      petroleum: 0.67,
+      naturalGas: 0.07,
+      biomass: 0.16,
+      electricity: 0.08,
+      // hydrogen: 0.02
+    },
+  },
+  unfinishedSymphony: {
+    // page 65
+    2014: {
+      petroleum: 0.92,
+      naturalGas: 0.04,
+      biomass: 0.03,
+      electricity: 0.01,
+    },
+    2030: {
+      petroleum: 0.87,
+      naturalGas: 0.03,
+      biomass: 0.08,
+      electricity: 0.02,
+    },
+    2060: {
+      petroleum: 0.60,
+      naturalGas: 0.06,
+      biomass: 0.21,
+      electricity: 0.10,
+      // hydrogen: 0.03
+    },
+  },
+  hardRock: {
+    // page 83
+    2014: {
+      petroleum: 0.92,
+      naturalGas: 0.04,
+      biomass: 0.03,
+      electricity: 0.01,
+    },
+    2030: {
+      petroleum: 0.90,
+      naturalGas: 0.04,
+      biomass: 0.04,
+      electricity: 0.02,
+    },
+    2060: {
+      petroleum: 0.78,
+      naturalGas: 0.07,
+      biomass: 0.10,
+      electricity: 0.04,
+      // hydrogen: 0.01
+    },
+  },
+};
+
+// TRANSPORTATION INTERPOLATION: For each plan, figure out the percent increase in each source.
+// We will interpolate the LLNL numbers by the corresponding percent increases.
+const TRANSPORT_PCT_INCREASES_BY_PLAN_AND_SOURCE = _.mapValues(TRANSPORT_SHARE, (planData, planKey) => ({
+  2030: _.mapValues(planData['2030'], (yearSource, sourceKey) =>
+    (yearSource * TRANSPORT_CONSUMPTION_MTOE[planKey][1]) /
+    (planData['2014'][sourceKey] * TRANSPORT_CONSUMPTION_MTOE[planKey][0])
+  ),
+  2060: _.mapValues(planData['2060'], (yearSource, sourceKey) =>
+    (yearSource * TRANSPORT_CONSUMPTION_MTOE[planKey][2]) /
+    (planData['2014'][sourceKey] * TRANSPORT_CONSUMPTION_MTOE[planKey][0])
+  ),
+}));
+
 /**
  * Interpolates energy usage in 2016 to electricity sources according to WEC scenarios report.
  * @param {object} link
  * @param {'modernJazz' | 'unfinishedSymphony' | 'hardRock'} wecScenarioKey Defaults to 'hardRock'
  */
 module.exports = function interpolateEnergyIn2060(link, wecScenarioKey = 'hardRock') {
-  if (link.target.id !== 'electricity') {
-    return link.data.energy2014.value.TWh;
+  if (link.target.id === 'electricity') {
+    let growthDefn = ELECTRICITY_SOURCE_2060_GROWTHS[link.source.id];
+
+    if (!growthDefn) {
+      throw new Error(`[model:wecWorldEnergyScenarios] Unable to find 2060 WEC electricity prediction for source '${link.source.id}'`);
+    }
+
+    return link.data.energy2014.value.TWh * growthDefn[wecScenarioKey];
   }
 
-  let growthDefn = ELECTRICITY_SOURCE_2060_GROWTHS[link.source.id];
-
-  if (!growthDefn) {
-    throw new Error(`[model:wecWorldEnergyScenarios] Unable to find 2060 WEC prediction for source '${link.source.id}'`);
+  if (link.target.id === 'residential' && link.source.id === 'solar') {
+    // No real residential breakdown, so interpolate off electricity growth for renewables.
+    return link.data.energy2014.value.TWh * ELECTRICITY_SOURCE_2060_GROWTHS.solar[wecScenarioKey];
   }
 
-  return link.data.energy2014.value.TWh * growthDefn[wecScenarioKey];
+  if (link.target.id === 'transportation') {
+    let growthDefn = _.get(TRANSPORT_PCT_INCREASES_BY_PLAN_AND_SOURCE, [wecScenarioKey, '2060', link.source.id]);
+
+    if (growthDefn === undefined) {
+      throw new Error(`[model:wecWorldEnergyScenarios] Unable to find 2060 WEC transportation prediction for source '${link.source.id}'`);
+    }
+
+    return link.data.energy2014.value.TWh * growthDefn;
+  }
+
+  return link.data.energy2014.value.TWh;
 };
